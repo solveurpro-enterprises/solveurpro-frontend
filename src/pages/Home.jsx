4b6ad2cb6c435e7043b3aeeee7274aa498aa22b2
Sameer khan <121482820+Sameerkhan9412@users.noreply.github.com>
2025-02-16 15:27:57 +0530

    add responsiveness in homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -19,8 +19,8 @@ const Home = () => {
   return (
     <div className=" mx-auto ">
       {/* hero section */}
-      <section className="flex items-center w-full max-xl:min-h-screen pt-8 relative bg-transparent lg:h-fit px-4 max-md:flex-col max-md:px-0  max-md:mx-2 lg:py-16 transition-all" >
-        <div className=" mt-8 relative max-w-7xl mx-auto ">
+      <section className="flex items-center justify-center w-full max-xl:min-h-screen pt-8 relative bg-transparent lg:h-fit px-4 max-md:flex-col max-md:px-0  max-md:mx-2 lg:py-16 transition-all" >
+        <div className=" mt-8 relative max-w-7xl mx-auto max-md:px-4">
           <p className="text-[var(--primary-color)] flex gap-4 items-center mb-2 text-sm">
             {" "}
             <FaBookOpen />
@@ -30,7 +30,7 @@ const Home = () => {
             <span className="font-light">Getting Best </span>
             <br /> <span className="font-bold"> Quality Education</span> <br /> <span>Is Now More Easier</span>
           </h1>
-          <p className="text-[var(--primary-color)]  mt-6 text-sm w-1/2">
+          <p className="text-[var(--primary-color)]  mt-6 text-sm w-1/2 max-md:w-full">
             An online tutor-providing platform where anyone can share their
             knowledge, and students can learn from industry experts. Offering
             flexible learning, one-on-one sessions, and expert guidance to
@@ -44,8 +44,8 @@ const Home = () => {
               <Button title="Get In Touch" buttonType={"secondary"} />
             </Link>
           </div>
-          <div className=" flex gap-4 mt-8">
-            <div className="flex gap-1 bg-[#726A61] py-1 px-2 rounded-lg items-center cursor-pointer hover:bg-[#726a61b2]">
+          <div className=" grid  gap-4 mt-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5">
+            <div className="flex gap-1 flex-wrap bg-[#726A61] py-1 px-2 rounded-lg items-center cursor-pointer hover:bg-[#726a61b2]">
                 <IoManSharp className="" />
               <div>Best Tutors</div>
             </div>
@@ -73,7 +73,7 @@ const Home = () => {
 
     <section className="px-4 max-w-7xl mx-auto">
       <h1 className={` border-l-4 pl-2 mt-4 border-l-red-500 text-black text-3xl font-bold mb-6`}>Our Services</h1>
-      <div className="grid grid-cols-5 gap-6 items-center justify-center text-center mb-4">
+      <div className="grid  gap-6 items-center justify-center text-center mb-4 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-5">
         <div className="flex flex-col gap-4 text-black items-center justify-center border-2 rounded-lg p-2 h-full border-gray-500 transition-all hover:-translate-y-4">
           <img src={tutor} alt="" className="w-16 object-cover"  />
           <h2 className="font-2xl font-bold">Best tutors</h2>
@@ -102,11 +102,11 @@ const Home = () => {
       </div>
     </section>
 
-    <section className="w-full flex flex-row my-12 h-fit items-center select-none group mx-4">
-      <div className=" overflow-hidden">
+    <section className="w-full max-w-7xl flex flex-col my-12 h-fit items-center select-none group mx-auto lg:flex-row ">
+      <div className=" overflow-hidden order-2 lg:order-1 ">
       <img src={aboutImg} className="w-full px-4 object-cover  transition-all duration-500 overflow-hidden group-hover:scale-110" alt="" />
       </div>
-      <div className="w-1/2 flex flex-col items-start ml-4">
+      <div className=" flex flex-col items-start ml-4 order-1 lg:order-2 lg:w-1/2 ">
         <h2 className="border-l-2 border-red-600 ml-2 pl-2 font-bold text-black text-3xl">About Us</h2>
         <p className="text-[#1F2937] p-4">
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laudantium accusamus animi expedita praesentium natus non! Tenetur, esse? Magni pariatur necessitatibus quibusdam ipsum labore omnis in beatae illum eum, quia maiores voluptatibus minus deleniti impedit corrupti eos placeat rerum? Magni molestiae obcaecati vel vitae commodi totam! Reiciendis voluptatum qui minima obcaecati quam, minus soluta distinctio nisi expedita, molestiae repellat fugit vel quisquam ipsum ipsam modi. Ut totam porro perspiciatis quasi esse, cumque perferendis dicta iure eaque quae repudiandae cupiditate molestias voluptatum quas veritatis dignissimos quo expedita commodi optio. Rem, beatae quo asperiores rerum laudantium, quas ipsa commodi dignissimos blanditiis possimus eveniet.

5434938b4e2b9ab41179ad8c84b02cd4e98e68c3
Sameer khan <121482820+Sameerkhan9412@users.noreply.github.com>
2025-02-16 14:54:43 +0530

    mobile nav added

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -1,35 +1,92 @@
-import React ,{useState}from 'react'
-import logo from "../../assets/logo.png"
-import { navLinks } from '../../utils/common-utils'
-import { Link } from 'react-router-dom'
-import Button from './Button'
+import React, { useState } from 'react';
+import logo from "../../assets/logo.png";
+import { navLinks } from '../../utils/common-utils';
+import { Link } from 'react-router-dom';
+import { FaBars, FaTimes } from 'react-icons/fa'; // Import icons for the hamburger menu
+
 const Navbar = () => {
-    const [active, setActive] = useState("Home");
+  const [active, setActive] = useState("Home");
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false); // State for mobile menu visibility
 
   const handleClick = (link) => {
     setActive(link);
+    setIsMobileMenuOpen(false); // Close mobile menu after clicking a link
   };
+
   return (
-    <div className='bg-white h-14 fixed w-full  z-40 px-4'>
-    <div className='  max-w-7xl h-14 mx-auto flex justify-between items-center ' >
-        <div className='h-full'><img className='h-full' src={logo} alt="" /></div>
-        <ul className='flex gap-6 text-[#686868]'>
-            {
-                navLinks.map((navLink,index)=>(
-                    <Link to={navLink.path} key={index} >
-                        <div className='w-full group relative  uppercase' onClick={() => handleClick(navLink.title)} >
-                            <div className={`relative py-1 after:content-[''] after:absolute after:inset-0 ${
-            active === navLink.title ? "after:border-b-4 after:border-red-500 after:-z-10 after:translate-y-2" : ""}`}>{navLink.title}</div>
-                            <div className=' absolute transition-all duration-500 rounded-lg -bottom-2 bg-red-500 h-1 w-0 group-hover:w-full'></div>
-                        </div>
-                    </Link>
-                ))
-            }
+    <nav className='bg-white h-14 fixed w-full z-40 px-4'>
+      <div className='max-w-7xl h-14 mx-auto flex justify-between items-center'>
+        {/* Logo */}
+        <div className='h-full'>
+          <img className='h-full' src={logo} alt="Logo" />
+        </div>
+
+        {/* Hamburger Menu Icon (Mobile Only) */}
+        <div className='md:hidden'>
+          <button
+            onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
+            className='text-gray-700 focus:outline-none'
+          >
+            {isMobileMenuOpen ? <FaTimes size={24} /> : <FaBars size={24} />}
+          </button>
+        </div>
+
+        {/* Desktop Menu */}
+        <ul className='hidden md:flex gap-6 text-[#686868]'>
+          {navLinks.map((navLink, index) => (
+            <li key={index}>
+              <Link to={navLink.path}>
+                <div
+                  className='w-full group relative uppercase'
+                  onClick={() => handleClick(navLink.title)}
+                >
+                  <div
+                    className={`relative py-1 after:content-[''] after:absolute after:inset-0 ${
+                      active === navLink.title
+                        ? "after:border-b-4 after:border-red-500 after:-z-10 after:translate-y-2"
+                        : ""
+                    }`}
+                  >
+                    {navLink.title}
+                  </div>
+                  <div className='absolute transition-all duration-500 rounded-lg -bottom-2 bg-red-500 h-1 w-0 group-hover:w-full'></div>
+                </div>
+              </Link>
+            </li>
+          ))}
         </ul>
-    </div>
-    </div>
 
-  )
-}
+        {/* Mobile Menu */}
+        {isMobileMenuOpen && (
+          <div className='md:hidden absolute top-14 left-0 w-full bg-white shadow-lg'>
+            <ul className='flex flex-col gap-4 p-4 text-[#686868]'>
+              {navLinks.map((navLink, index) => (
+                <li key={index}>
+                  <Link to={navLink.path}>
+                    <div
+                      className='w-full group relative uppercase'
+                      onClick={() => handleClick(navLink.title)}
+                    >
+                      <div
+                        className={`relative py-1 after:content-[''] after:absolute after:inset-0 ${
+                          active === navLink.title
+                            ? "after:border-b-4 after:border-red-500 after:-z-10 after:translate-y-2"
+                            : ""
+                        }`}
+                      >
+                        {navLink.title}
+                      </div>
+                      <div className='absolute transition-all duration-500 rounded-lg -bottom-2 bg-red-500 h-1 w-0 group-hover:w-full'></div>
+                    </div>
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
